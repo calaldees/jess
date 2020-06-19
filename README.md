109957af12f8020692de842a11bf6296194c8ec8
Allan Callaghan <calaldees@gmail.com>
2020-06-19 21:09:54 +0100

    Chess board draws

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ A simple experiment to write an javascript browser based realtime multiplayer ch
 
 https://www.fastemoji.com/%E2%99%9A-%E2%99%9B-%E2%99%9C-%E2%99%9D-%E2%99%9E-%E2%99%9F-%E2%99%94-%E2%99%95-%E2%99%96-%E2%99%97-%E2%99%98-%E2%99%99-Meaning-Emoji-Emoticon-Chess-Ascii-Art-Hobby-Symbols-King-Japanese-Kaomoji-Smileys-7823.html
 
+
+https://www.compart.com/
--- jess.js
+++ jess.js
@@ -1,3 +1,49 @@
+// State -----------------------------------------------------------------------
+
+let state = {
+    layers: [
+        '□■□■□■□■' +
+        '■□■□■□■□' +
+        '□■□■□■□■' +
+        '■□■□■□■□' +
+        '□■□■□■□■' +
+        '■□■□■□■□' +
+        '□■□■□■□■' +
+        '■□■□■□■□'
+        ,
+        '♖♘♗♕♔♗♘♖' +
+        '♙♙♙♙♙♙♙♙' +
+        '        ' +
+        '        ' +
+        '        ' +
+        '        ' +
+        '♟♟♟♟♟♟♟♟' +
+        '♜♞♝♛♚♝♞♜'
+        ,
+        '        ' +
+        '        ' +
+        '        ' +
+        '        ' +
+        '        ' +
+        '        ' +
+        '        ' +
+        '        '
+    ],
+    meta: {
+        width: 8,
+        height: 8,
+    },
+}
+
+
+
+// Canvas ----------------------------------------------------------------------
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d', { alpha: false });
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
 // Websocket -------------------------------------------------------------------
 window.addEventListener('hashchange', ()=>{window.location.reload()});
 const channel = window.location.hash.replace('#','');
@@ -12,16 +58,57 @@ socket.addEventListener('close', function (event) {
 });
 socket.addEventListener('message', function (event) {
     console.log('message');
+    state = JSON.parse(event.data);
+    drawDisplay();
 });
 
 
-// Canvas ----------------------------------------------------------------------
-const htmlCanvas = document.getElementById("canvas");
+// Input -----------------------------------------------------------------------
 
-htmlCanvas.addEventListener('mousedown', (event) => {
-    const rect = htmlCanvas.getBoundingClientRect()
+canvas.addEventListener('mousedown', (event) => {
+    const rect = canvas.getBoundingClientRect()
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
     console.log("x: " + x + " y: " + y);
 })
 
+
+// Game Logic ------------------------------------------------------------------
+
+function drawDisplay() {
+    const tile_width = canvas.width / state.meta.width;
+    const tile_height = canvas.height / state.meta.height;
+
+    ctx.font = `${Math.min(tile_width, tile_height)}px serif`;
+    ctx.textBaseline = 'middle';
+    ctx.textAlign = 'center';
+
+    function drawTile(x, y, char) {
+        x = x * tile_width;
+        y = y * tile_height;
+        if (char == '□') {
+            ctx.fillStyle = 'white';
+            ctx.fillRect(x, y, tile_width, tile_height);
+        }
+        else if (char == '■') {
+            ctx.fillStyle = 'black';
+            ctx.fillRect(x, y, tile_width, tile_height);
+        }
+        else if (char == ' ') {
+            // nothing
+        }
+        else {
+            ctx.fillStyle = 'orange';
+            ctx.fillText(char, x + (tile_width/2), y + (tile_height/2), tile_width);
+        }
+    }
+
+    for (let layer of state.layers) {
+        for (var index=0; index<layer.length; index++) {
+            let [x, y] = [index % state.meta.height, Math.floor(index/state.meta.height)];
+            drawTile(x, y, layer[index]);
+        }
+    }
+}
+
+drawDisplay();
