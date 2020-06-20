ace738977ae6574c4546f5ac98fd16b5d7aaa06f
Allan Callaghan <calaldees@gmail.com>
2020-06-20 12:22:35 +0100

    Starting tidying Socket can now reconnect Window can be resized Functions that used generic zip/all/enumerate have been simplified for strings Rendering slightly tidyed

--- jess.js
+++ jess.js
@@ -1,42 +1,27 @@
 // Utils -----------------------------------------------------------------------
 
 // https://stackoverflow.com/a/1431113/3356840
-function replaceAt(string, index, replacement) {
+function strReplaceAt(string, index, replacement) {
     return string.substr(0, index) + replacement + string.substr(index + replacement.length);
 }
 
-function all(iterable) {
-    for (let i of iterable) {
-        if (!i) {return false;}
-    }
-    return true;
-}
-function* zip(...iterables) {
-    const iterators = [...iterables].map(iterable => iterable[Symbol.iterator]());
-    while (true) {
-        const iterable_items = iterators.map(iterator => iterator.next());
-        if (all(iterable_items.map(i => i.done))) {break;}
-        yield iterable_items.map(i => i.value);
-    }
-}
-function* enumerate(iterable) {
-    let count = 0;
-    for (let item of iterable) {
-        yield [count++, item];
+function* strZip(aa, bb) {
+    console.assert(aa.length == bb.length, 'string length must match');
+    for (let i=0 ; i < aa.length ; i++) {
+        yield [aa.charAt(i), bb.charAt(i)];
     }
 }
 
-// this
-
-function* diffStrIndexs(aa, bb) {
-    console.assert(aa.length == bb.length, 'string length must match for diffStr to work');
+function* strDiffIndexs(aa, bb) {
+    console.assert(aa.length == bb.length, 'string length must match');
     for (let i=0 ; i < aa.length ; i++) {
         if (aa.charAt(i) != bb.charAt(i)) {
             yield i;
         }
     }
 }
-function countChars(string, chars) {
+
+function strCountChars(string, chars) {
     let count = 0;
     for (let i=0 ; i<string.length ; i++) {
         if (chars.indexOf(string.charAt(i)) >= 0) {
@@ -61,7 +46,7 @@ const CHESS_PIECE_COLOR_WHITE = '#eeee88';
 const CHESS_PIECE_COLOR_BLACK = '#46468c';
 const CHESS_PIECES_WHITE = '♖♘♗♕♔♙';
 const CHESS_PIECES_BLACK = '♜♞♝♛♚♟';
-const CHESS_PIECE_COLOR_INVERSION_LOOKUP = new Map(zip(
+const CHESS_PIECE_COLOR_INVERSION_LOOKUP = new Map(strZip(
     CHESS_PIECES_WHITE + CHESS_PIECES_BLACK,
     CHESS_PIECES_BLACK + CHESS_PIECES_WHITE,
 ));
@@ -111,12 +96,18 @@ let state = {
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext('2d', { alpha: false });
 
-// Resize (could be put in function later)
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-const tile_width = canvas.width / state.meta.width;
-const tile_height = canvas.height / state.meta.height;
-ctx.font = `${Math.min(tile_width, tile_height)}px serif`;
+let tile_width;
+let tile_height;
+
+function resizeWindow() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    tile_width = canvas.width / state.meta.width;
+    tile_height = canvas.height / state.meta.height;
+    ctx.font = `${Math.min(tile_width, tile_height)}px serif`;
+    drawDisplay();
+}
+window.addEventListener('resize', resizeWindow);
 
 
 // Websocket -------------------------------------------------------------------
@@ -125,35 +116,55 @@ window.addEventListener('hashchange', ()=>{window.location.reload()});
 const channel = window.location.hash.replace('#','');
 const ws_url = `ws://${window.location.hostname}:9800/${channel}.ws`;
 
-const socket = new WebSocket(ws_url);
-socket.addEventListener('open', function (event) {
-    console.log('open');
-});
-socket.addEventListener('close', function (event) {
-    console.log('close');
-});
-socket.addEventListener('message', function (event) {
-    console.log('message');
-    state = JSON.parse(event.data);
+let socket;
+function socketConnect() {
+    if (socket) {
+        console.warn('socket already connected');
+        return;
+    }
+    console.log('socket create', ws_url);
+    socket = new WebSocket(ws_url);
+    socket.addEventListener('open', function (event) {
+        console.log('socket open');
+    });
+    socket.addEventListener('close', function (event) {
+        console.log('socket close');
+        socket = undefined;
+    });
+    socket.addEventListener('message', function (event) {
+        console.debug('socket message');
+        receiveState(JSON.parse(event.data));
+    });
+}
+
+function receiveState(_state) {
+    state = _state;
     drawDisplay();
-});
-function sendState() {
-    socket.send(JSON.stringify(state));
+}
+
+function sendState(_state) {
+    if (socket && socket.readyState == 1) {
+        socket.send(JSON.stringify(_state));
+        // this will trigger socker.message -> receiveState -> drawDisplay
+    }
+    else {
+        console.warn('Not connected to websocket');
+        socketConnect();  // attempt reconnect
+        receiveState(_state);  // manually push state
+    }
 }
 
 
 // Input -----------------------------------------------------------------------
 
 canvas.addEventListener('mousedown', (event) => {
-    let [x, y] = [
-        event.clientX - canvas.getBoundingClientRect().left,
-        event.clientY - canvas.getBoundingClientRect().top,
-    ];
+    let x = event.clientX - canvas.getBoundingClientRect().left;
+    let y = event.clientY - canvas.getBoundingClientRect().top;
     x = Math.floor(x/tile_width);
     y = Math.floor(y/tile_height);
     //console.log("x: " + x + " y: " + y);
     updateState(x, y);
-    sendState();
+    sendState(state);
 });
 
 
@@ -163,15 +174,15 @@ function updateState(x, y) {
     const i2 = x + (y * state.meta.width);
     const blank_layer = BLANK_CHAR.repeat(state.meta.width * state.meta.height);
     const active_layer = state.layers[1];
-    const active_count = countChars(active_layer, ACTIVE_CHAR);
+    const active_count = strCountChars(active_layer, ACTIVE_CHAR);
     if (active_count == 0) {
-        state.layers[1] = replaceAt(active_layer, i2, ACTIVE_CHAR);
+        state.layers[1] = strReplaceAt(active_layer, i2, ACTIVE_CHAR);
     }
     if (active_count == 1) {
-        const i1 = [...diffStrIndexs(blank_layer, active_layer)][0];
+        const i1 = [...strDiffIndexs(blank_layer, active_layer)][0];
         const char = state.layers[2].charAt(i1);
-        state.layers[2] = replaceAt(state.layers[2], i1, BLANK_CHAR);
-        state.layers[2] = replaceAt(state.layers[2], i2, char);
+        state.layers[2] = strReplaceAt(state.layers[2], i1, BLANK_CHAR);
+        state.layers[2] = strReplaceAt(state.layers[2], i2, char);
         state.layers[1] = blank_layer;
     }
 }
@@ -182,9 +193,13 @@ function updateState(x, y) {
 function drawTile(x, y, char) {
     x = x * tile_width;
     y = y * tile_height;
+    const x_center = x + (tile_width/2);
+    const y_center = y + (tile_height/2);
     ctx.lineWidth = tile_width / 64;
 
-    if (char == '□') {
+    if (char == BLANK_CHAR) {
+    }
+    else if (char == '□') {
         ctx.fillStyle = CHESS_BOARD_COLOR_WHITE;
         ctx.fillRect(x, y, tile_width, tile_height);
     }
@@ -194,17 +209,14 @@ function drawTile(x, y, char) {
     }
     else if (char == ACTIVE_CHAR) {
         ctx.fillStyle = ACTIVE_CHAR_COLOR;
-        ctx.fillRect(x, y, tile_width, tile_height);
         ctx.strokeStyle = ACTIVE_CHAR_COLOR_BORDER;
+        ctx.fillRect(x, y, tile_width, tile_height);
         ctx.strokeRect(x, y, tile_width, tile_height);
-
     }
-    else if (char == ' ') {
-        // nothing
-    }
-    else {
+    else {  // All other chars
         ctx.textBaseline = 'middle';
         ctx.textAlign = 'center';
+        ctx.strokeStyle = 'black';
 
         if (CHESS_PIECES_BLACK.indexOf(char) >= 0) {
             ctx.fillStyle = CHESS_PIECE_COLOR_BLACK;
@@ -214,19 +226,16 @@ function drawTile(x, y, char) {
             char = CHESS_PIECE_COLOR_INVERSION_LOOKUP.get(char);
         }
 
-        const xc = x + (tile_width/2);
-        const yc = y + (tile_height/2);
-        ctx.fillText(char, xc, yc);
-        ctx.strokeStyle = 'black';
-        ctx.strokeText(char, xc, yc);
+        ctx.fillText(char, x_center, y_center);
+        ctx.strokeText(char, x_center, y_center);
     }
 }
 
 function drawDisplay() {
     for (let layer of state.layers) {
-        for (var index=0; index<layer.length; index++) {
-            let [x, y] = [index % state.meta.height, Math.floor(index/state.meta.height)];
-            drawTile(x, y, layer[index]);
+        for (var i=0; i<layer.length; i++) {
+            let [x, y] = [i % state.meta.height, Math.floor(i/state.meta.height)];
+            drawTile(x, y, layer[i]);
         }
     }
 }
@@ -234,4 +243,5 @@ function drawDisplay() {
 
 // Main ------------------------------------------------------------------------
 
-drawDisplay();
+socketConnect();
+resizeWindow();
